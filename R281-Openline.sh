835573e3237ca25abf8922ca78f5d9d356455c4a
CJGonzales13 <98582948+CJGonzales13@users.noreply.github.com>
2022-02-13 13:45:05 +0800

    Update R281-Openline.sh

--- R281-Openline.sh
+++ R281-Openline.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Openline, Band Locking, Disable Updates..."
-wget https://github.com/marukoy/R281/raw/main/r281_v4.2_openline.bin -O /tmp/firmware.bin
+wget https://github.com/CJGonzales13/R281_Openline/raw/main/r281_v4.2_openline.bin -O /tmp/firmware.bin
 echo "Done! Wait for the modem to reboot..."
 mtd -r write /tmp/firmware.bin /dev/mtd5 > /dev/null 2&>1
